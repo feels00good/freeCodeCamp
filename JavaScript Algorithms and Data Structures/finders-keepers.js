387371916ca1ad595f0d1748721431232985abf2
feels00good <feels00good@ka-as.ru>
2018-05-30 18:42:26 +0300

    Finders Keepers

---
+++ JavaScript Algorithms and Data Structures/finders-keepers.js
@@ -0,0 +1,12 @@
+/* Basic Algorithm Scripting:
+> Finders Keepers
+
+Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument). If no element passes the test, return undefined.
+ */
+
+
+function findElement(arr, func) {
+  return arr.filter(func)[0];
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
